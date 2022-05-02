3834be9559a719f3b5ab21058f969a7f4f767d01
JueCheng <JueCheng@Fly.com>
2022-05-02 11:14:03 +0800

    second-submit

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,13 +1,30 @@
 <template>
-      <view>Cart</view>
+<view></view>
 </template>
 
 <script>
 export default {
+data() {
+return {
 
 }
-</script>
+},
+  onLoad: function (options) {
 
-<style>
+},
+  onReady: function () {
+
+},
+//下拉 刷新事件
+  onPullDownRefresh: function () {
+
+},
+//上拉 触底事件
+    onReachBottom: function () {
+
+},
+}
+</script>
+<style lang='scss' scoped>
 
 </style>
